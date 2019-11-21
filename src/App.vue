1564ea75800496f0468b7d54c54a5e778b1355bb
Neill <540311007@qq.com>
2019-11-21 12:55:45 +0800

    又调了一下

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,12 @@
 <template>
   <div>
-    <h1>HELLOW WORD</h1>
-    <Table stripe :columns="columns1" :data="data1"></Table>
-     <Row>
-        <Col span="12">
-            <DatePicker type="date" placeholder="Select date" style="width: 200px"></DatePicker>
-        </Col>
-        <Col span="12">
-            <DatePicker type="daterange" placement="bottom-end" placeholder="Select date" style="width: 200px"></DatePicker>
-        </Col>
-    </Row>
-    <Button @click="value1 = true" type="primary">Open</Button>
-    <Drawer title="Basic Drawer" :closable="false" v-model="value1">
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-    </Drawer>
+    <h1>你好世界</h1>
   </div>
 </template>
 
 <script>
   export default {
-    data () {
-            return {
-                 value1: false,
-                columns1: [
-                    {
-                        title: 'Name',
-                        key: 'name'
-                    },
-                    {
-                        title: 'Age',
-                        key: 'age'
-                    },
-                    {
-                        title: 'Address',
-                        key: 'address'
-                    }
-                ],
-                data1: [
-                    {
-                        name: 'John Brown',
-                        age: 18,
-                        address: 'New York No. 1 Lake Park',
-                        date: '2016-10-03'
-                    },
-                    {
-                        name: 'Jim Green',
-                        age: 24,
-                        address: 'London No. 1 Lake Park',
-                        date: '2016-10-01'
-                    },
-                    {
-                        name: 'Joe Black',
-                        age: 30,
-                        address: 'Sydney No. 1 Lake Park',
-                        date: '2016-10-02'
-                    },
-                    {
-                        name: 'Jon Snow',
-                        age: 26,
-                        address: 'Ottawa No. 2 Lake Park',
-                        date: '2016-10-04'
-                    }
-                ]
-            }
-        }
+                  
   }
 </script>
 
